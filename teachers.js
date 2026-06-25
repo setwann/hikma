52a273ffcbddf3d5520194c6d6a17b911e79bc55
setwann <myrampro@gmail.com>
2026-06-25 03:58:18 +0300

    Create teachers.js

---
+++ teachers.js
@@ -0,0 +1,13 @@
+[
+  {
+    "fullName": "اركان دارا رشید",
+    "phone": "07701217949",
+    "address": "کانی با",
+    "job": "",
+    "ijaza": "بەڵێ",
+    "ijazaDetail": "",
+    "subjects": [],
+    "assignedClasses": [],
+    "students": []
+  }
+]
